d63cf7a3d154f9c7b4c8163db5a5e10e545728de
Anastasiya <a.rashinskaya@gmail.com>
2019-02-02 19:16:55 +0300

    Added auth

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
 const winston = require('./routes/logger');
+const passport = require('./model/authStrategy');
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
-// const login = require('./routes/loginPage')
 
 var app = express();
 
-// app.get('/auth/facebook',
-//   passport.authenticate('facebook'));
-
-// app.get('/auth/facebook/callback',
-//   passport.authenticate('facebook', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     // Successful authentication, redirect home.
-//     res.redirect('/');
-//   });
+app.use(passport.initialize());
+app.get('/',
+  passport.authenticate('local'),
+  function (req, res) {
+    res.redirect('/index')
+  }
+);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -33,7 +31,7 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/news', newsRouter);
 
 app.use(function (req, res, next) {
---
+++ model/authStrategy.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const db = require('../model/usersSchema');
+const Strategy = require('passport-local').Strategy;
+
+passport.use(new Strategy(
+    function (username, password, cb) {
+        db.findByUsername(username, function (err, user) {
+            if (err) { return cb(err); }
+            if (!user) { return cb(null, false); }
+            if (user.password != password) { return cb(null, false); }
+            return cb(null, user);
+        });
+    }));
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    db.users.findById(id, function (err, user) {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
+module.exports = passport;
