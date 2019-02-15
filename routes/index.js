cc711c8429a967893a506e0576199abdb5f1db1e
Anastasiya Rashchynskaya <Anastasiya_Rashchynskaya@epam.com>
2019-02-15 17:20:45 +0300

    Removed call to news api

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,30 @@ const fetch = require("node-fetch");
 const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=e25f68463ae0441a947aadda3a0fa55c';
 
 router.route('/').get((req, res) => {
-  fetch(url).then(response =>
-    response.json().then(data => ({
-      data: data,
-      status: response.status
-    })).then(res => {
-      const articleArr = res.data.articles || [];
-      articleArr.forEach(article => {
-        const newsFromApi = new News({
-          source: {
-            id: article.source.id,
-            name: article.source.name
-          },
-          author: article.author,
-          title: article.title,
-          description: article.description,
-          url: article.url,
-          urlToImage: article.urlToImage,
-          publishedAt: article.publishedAt,
-          content: article.content
-        });
-        newsFromApi.save();
-      })
-    })).then(res.render('index',{title:'news'}))
+  // fetch(url).then(response =>
+  //   response.json().then(data => ({
+  //     data: data,
+  //     status: response.status
+  //   })).then(res => {
+  //     const articleArr = res.data.articles || [];
+  //     articleArr.forEach(article => {
+  //       const newsFromApi = new News({
+  //         source: {
+  //           id: article.source.id,
+  //           name: article.source.name
+  //         },
+  //         author: article.author,
+  //         title: article.title,
+  //         description: article.description,
+  //         url: article.url,
+  //         urlToImage: article.urlToImage,
+  //         publishedAt: article.publishedAt,
+  //         content: article.content
+  //       });
+  //       newsFromApi.save();
+  //     })
+  //   })).then(
+    res.render('index',{title:'news'})
 });
 
 module.exports = router;
